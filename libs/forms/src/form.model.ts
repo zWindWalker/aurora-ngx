abffe263764da61f1587fc0bd29cb3d4e146f8e3
wind.walker <hieu.huynh@apps-cyclone.com>
2018-11-20 18:16:12 +0700

    sdfsa

--- libs/forms/src/form.model.ts
+++ libs/forms/src/form.model.ts
@@ -1,23 +1,27 @@
-import { TemplateRef } from '@angular/core';
+import {TemplateRef} from '@angular/core';
 
 export interface AuroraForm {
-  type?: string,
-  name: string,
-  placeholder?: string,
-  label?: string,
-  value?: any,
-  input_type?: string,
-  checkbox_label?: string,
-  properties?: Object,
-  validators?: Array<string>,
-  options?: Array<{ value: any, label: any, [property: string]: any }>,
-  feedback?: Object,
-  hidden?: Boolean,
-  template?: TemplateRef<any>,
+    type?: string,
+    name: string,
+    placeholder?: string,
+    label?: string,
+    value?: any,
+    input_type?: string,
+    checkbox_label?: string,
+    properties?: {
+        [name: string]: any
+    },
+    validators?: Array<string>,
+    options?: Array<{ value: any, label: any, [property: string]: any }>,
+    feedback?: {
+        [name: string]: any
+    },
+    hidden?: Boolean,
+    template?: TemplateRef<any>,
 
-  [property: string]: any
+    [property: string]: any
 }
 
 export interface AuroraFormTemplate {
-  [name: string]: Array<string>
+    [name: string]: Array<string>
 }

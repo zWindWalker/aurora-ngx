fe56180ed60ee35f9b974a345bbfc4c59d0495e7
zHieuHuynh <hieuht0812@gmail.com>
2018-11-27 08:57:47 +0700

    no message

--- apps/homeey/src/app/features/auth/pages/forgot-password/forgot-password.module.ts
+++ apps/homeey/src/app/features/auth/pages/forgot-password/forgot-password.module.ts
@@ -1,20 +1,20 @@
-import {NgModule} from '@angular/core';
+import { NgModule } from '@angular/core';
 
-import {ForgotPasswordComponent} from './forgot-password.component';
-import {SharedModule} from "../../../../shared/shared.module";
-import {RouterModule} from "@angular/router";
+import { ForgotPasswordComponent } from './forgot-password.component';
+import { SharedModule } from '../../../../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-    imports: [
-        SharedModule,
-        RouterModule.forChild([{
-            path: '', component: ForgotPasswordComponent
-        }])
-    ],
-    declarations: [
-        ForgotPasswordComponent
-    ]
+  imports: [
+    SharedModule,
+    RouterModule.forChild([{
+      path: '', component: ForgotPasswordComponent
+    }])
+  ],
+  declarations: [
+    ForgotPasswordComponent
+  ]
 })
 export class ForgotPasswordModule {
 }
